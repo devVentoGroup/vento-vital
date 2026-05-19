92d43bfac1f42d379928608005375a4b09bb04a4
carlos alejandro ibarra ariza <dev.ventogroup@gmail.com>
2026-05-19 15:32:05 -0500

    Remove local Supabase project

--- supabase/migrations/20260310143500_app_content_blocks_trigger_fix.sql
+++
@@ -1,8 +0,0 @@
-begin;
-
-drop trigger if exists app_content_blocks_set_updated_at on public.app_content_blocks;
-create trigger app_content_blocks_set_updated_at
-before update on public.app_content_blocks
-for each row execute function public.update_updated_at();
-
-commit;
--- supabase/migrations/20260314123000_viso_public_pass_satellites_view_grants.sql
+++
@@ -1,7 +0,0 @@
-begin;
-
-grant select, insert, update, delete
-on public.pass_satellites
-to authenticated;
-
-commit;
